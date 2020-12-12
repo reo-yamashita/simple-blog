84ae796bba75bfb62a7213ad19655dad769896c3
nako0215 <reo.akarinibu0215@gmail.com>
2020-12-12 14:02:50 +0900

    add material icon plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-postcss`,
+    `gatsby-plugin-material-ui`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
