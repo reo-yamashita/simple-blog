fda5055216e6f0bbfd2b2fd81fa8dbe19b7a752d
nako0215 <reo.akarinibu0215@gmail.com>
2020-12-16 11:05:43 +0900

    fix setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      // Build your palette here
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      orange: colors.orange,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      lightblue: colors.lightBlue,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
     extend: {},
   },
   variants: {},
