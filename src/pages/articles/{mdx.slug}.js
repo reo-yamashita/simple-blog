72a1df7d66a478282bf33d792be9e8e2e82ab7ca
nako0215 <reo.akarinibu0215@gmail.com>
2020-12-16 12:58:05 +0900

    new Route API

--- src/pages/articles/{mdx.slug}.js
+++ src/pages/articles/{mdx.slug}.js
@@ -5,8 +5,8 @@ import Layout from "@layouts/main_layout"
 import Seo from "@components/Seo"
 
 export const query = graphql`
-  query Article($slug: String!) {
-    post: mdx(fields: { slug: { eq: $slug } }) {
+  query Post ($slug: String!) {
+    mdx (slug: { eq: $slug }) {
       fields {
         slug
       }
@@ -22,20 +22,21 @@ export const query = graphql`
 
 const Blog = ({ data, pageContext }) => {
   // const page_arrow = pageContext
+  console.log(data)
   return (
     <Layout>
       <Seo title={"blog"} description={"article"} author={"Near String"} />
       <div className="px-4 py-12 flex-1 md:px-12">
         <h2 className="select-none inline-block text-2xl sm:text-3xl font-bold tracking-wide border-b-0">
-          {data.post.frontmatter.title}
+          {data.mdx.frontmatter.title}
         </h2>
         <div className="flex items-center leading-none">
           <p className="text-sm select-none text-gray-800 opacity-75s pt-1 pl-3">
-            posted on {data.post.frontmatter.date}
+            posted on {data.mdx.frontmatter.date}
           </p>
         </div>
         <div className="p-3 text-sm sm:text-base text-black">
-          <MDXRenderer>{data.post.body}</MDXRenderer>
+          <MDXRenderer>{data.mdx.body}</MDXRenderer>
         </div>
         {/* <PageMove  arrow={page_arrow}/> */}
       </div>
